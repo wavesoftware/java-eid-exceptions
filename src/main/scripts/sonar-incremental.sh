d259b1140d2102a21167c45d6206a244318393a8
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2015-11-22 15:55:30 +0100

    Remove unneeded sonar incremental file

--- src/main/scripts/sonar-incremental.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-unset MAVEN_OPTS
-unset JAVA_TOOL_OPTIONS
-exec mvn -P ci sonar:sonar \
-    -D skipTests \
-    -D sonar.language=java \
-    -D sonar.analysis.mode=incremental \
-    -D sonar.host.url=https://sonar.wavesoftware.pl \
-    -D sonar.preview.excludePlugins=buildstability,devcockpit,pdfreport,report,views,jira,buildbreaker,issueassign,scmstats
